2091d4f465ed22aa66899842d730930ae20c5439
LiuFeng <lf18827118119@qq.com>
2019-04-01 11:53:40 +0800

    改变Hello → HelloWorld

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@
  * 作者: 刘枫
  * 描述:
  * 日期: 2019年04月01日
- * @author
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("HelloWorld");
     }
 }
